e057f931186870bd3aa000d304c5a94fba97e8e5
Aditya Srivastava <adityasrivastava9406@gmail.com>
2024-08-14 16:05:38 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,11 @@ app.use('/uploads', express.static(path.join(__dirname, '../ignore/uploads')));
 app.use('/api/posts', require('../backend/routes/postRouters'));
 app.use('/api/users', require('../backend/routes/userRoutes'))
 app.use('/api/user', require('../backend/routes/user'));
+if (process.env.NODE_ENV=="production"){
+    const dirPath = path.resolve()
+    app.use(express.static('Frontend/dist'))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath,"Frontend","dist","index.html"))
+    })
+}
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
